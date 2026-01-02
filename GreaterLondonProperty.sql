fa52ae4fcfc8aa24616162769134796466f4e22b
Shayni Ravishika Ruparathna <shainyravishika6464@gmail.com>
2026-01-02 11:09:31 +0530

    Add files via upload

---
+++ GreaterLondonProperty.sql
@@ -0,0 +1,73 @@
+select *
+from [SQL task 1].[dbo].[pp-2020]
+
+UPDATE dbo.[pp-2020]
+ SET Locality='0'
+ FROM [SQL task 1].[dbo].[pp-2020]
+ WHERE Locality IS NULL
+
+ UPDATE dbo.[pp-2020]
+ SET SAON='0'
+ FROM [SQL task 1].[dbo].[pp-2020]
+ WHERE SAON IS NULL
+
+ UPDATE dbo.[pp-2020]
+ SET Street='0'
+ FROM [SQL task 1].[dbo].[pp-2020]
+ WHERE Street IS NULL
+
+
+ select *
+from [SQL task 1].[dbo].[pp-2021]
+
+UPDATE dbo.[pp-2021]
+ SET Locality='0'
+ FROM [SQL task 1].[dbo].[pp-2021]
+ WHERE Locality IS NULL
+
+ UPDATE dbo.[pp-2021]
+ SET SAON='0'
+ FROM [SQL task 1].[dbo].[pp-2021]
+ WHERE SAON IS NULL
+
+ UPDATE dbo.[pp-2021]
+ SET Street='0'
+ FROM [SQL task 1].[dbo].[pp-2021]
+ WHERE Street IS NULL
+
+ select *
+from [SQL task 1].[dbo].[pp-2022]
+
+UPDATE dbo.[pp-2022]
+ SET Locality='0'
+ FROM [SQL task 1].[dbo].[pp-2022]
+ WHERE Locality IS NULL
+
+ UPDATE dbo.[pp-2022]
+ SET SAON='0'
+ FROM [SQL task 1].[dbo].[pp-2022]
+ WHERE SAON IS NULL
+
+ UPDATE dbo.[pp-2022]
+ SET Street='0'
+ FROM [SQL task 1].[dbo].[pp-2022]
+ WHERE Street IS NULL
+
+ select *
+from [SQL task 1].[dbo].[pp-2023]
+
+UPDATE dbo.[pp-2023]
+ SET Locality='0'
+ FROM [SQL task 1].[dbo].[pp-2023]
+ WHERE Locality IS NULL
+
+ UPDATE dbo.[pp-2023]
+ SET SAON='0'
+ FROM [SQL task 1].[dbo].[pp-2023]
+ WHERE SAON IS NULL
+
+ UPDATE dbo.[pp-2023]
+ SET [Street]='0'
+ FROM [SQL task 1].[dbo].[pp-2023]
+ WHERE [Street] IS NULL
+
